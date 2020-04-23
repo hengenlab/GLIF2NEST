a21265ae01b3f9c6aea9c92df13f8ded58a337a5
kbn-gh <kbn.git@gmail.com>
2020-04-23 12:45:50 -0500

    fix get_delay to get_delay_steps, based on https://github.com/nest/nest-simulator/issues/1025

--- GlifModel/glif_lif_asc.cpp
+++ GlifModel/glif_lif_asc.cpp
@@ -293,7 +293,7 @@ nest::glif_lif_asc::update( Time const& origin, const long from, const long to )
 void
 nest::glif_lif_asc::handle( SpikeEvent& e )
 {
-  assert( e.get_delay() > 0 );
+  assert( e.get_delay_steps() > 0 );
 
   B_.spikes_.add_value(
     e.get_rel_delivery_steps( kernel().simulation_manager.get_slice_origin() ),
@@ -303,7 +303,7 @@ nest::glif_lif_asc::handle( SpikeEvent& e )
 void
 nest::glif_lif_asc::handle( CurrentEvent& e )
 {
-  assert( e.get_delay() > 0 );
+  assert( e.get_delay_steps() > 0 );
 
   B_.currents_.add_value(
     e.get_rel_delivery_steps( kernel().simulation_manager.get_slice_origin() ),
